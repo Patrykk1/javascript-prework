4044732ad718f310566c40c55739e9e950e9bfc5
Patryk Pułkotycki <patryk.pulkotycki@wp.pl>
2020-06-08 13:08:01 +0200

    functions

---
+++ jsfunctions.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+function printMessage(msg) {
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages() {
+	document.getElementById('messages').innerHTML = '';
+}
