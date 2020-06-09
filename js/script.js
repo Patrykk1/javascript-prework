53dc413758ead224636f5d4ef03ae19a2eb0a460
Patryk Pułkotycki <patryk.pulkotycki@wp.pl>
2020-06-09 16:12:56 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// JavaScript source code
+
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+let playerMove = 'papier';
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    playerMove = 'kamieñ';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+if (randomNumber == 2) {
+    playerMove = 'papier';
+}
+printMessage('Mój ruch to: ' + computerMove);
+if (randomNumber == 3) {
+    playerMove = 'no¿yce';
+}
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamieñ, 2: papier, 3: no¿yce.');
+
+console.log('Gracz wpisa³: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamieñ';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+if (playerInput == '2') {
+    playerMove = 'papier';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+if (playerInput == '3') {
+    playerMove = 'no¿yce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+}
+    if else (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+    }
+    if else (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+    }
+    if else (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+    }
+    if else (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis!');
+    }
+    if else (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis!');
+    }
+    if else (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis!');
+    }
+    else
+    printMessage('Ty przegrywasz')
