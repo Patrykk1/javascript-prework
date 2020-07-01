a31648f2a3ae6b0937a54e50aefe8bf60815e444
Patrykk1 <66215919+Patrykk1@users.noreply.github.com>
2020-07-01 14:03:44 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,46 @@
 // JavaScript source code
-function printMessage(msg) {
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
+function playGame(){
+	function printMessage(msg) {
+	 let div = document.createElement('div');
+	 div.innerHTML = msg;
+	 document.getElementById('messages').appendChild(div);
+	}
 
-function clearMessages() {
-	document.getElementById('messages').innerHTML = '';
-}
+	function clearMessages() {
+	 document.getElementById('messages').innerHTML = '';
+	}
 
-function getMoveName(argMoveId){
-  if(argMoveId == 1){
-    return 'kamień';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch';
-  }
-  if(argMoveId == 2){
-    return 'papier';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch';
-  }
-  if(argMoveId == 3){
-    return 'nożyce';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch';
-  }
+	function getMoveName(argMoveId){
+  	 if(argMoveId == 1){
+          return 'kamień';
+  	 } else {
+    	  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    	  return 'nieznany ruch';
+  	}
+  	 if(argMoveId == 2){
+    	  return 'papier';
+  	  } else {
+    	  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    	  return 'nieznany ruch';
+  	}
+  	if(argMoveId == 3){
+    	 return 'nożyce';
+  	 } else {
+    	 printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    	 return 'nieznany ruch';
+  	}
+
+	function displayResult(argComputerMove, argPlayerMove){
+  	 printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+
+  	 if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    	  printMessage('Ty wygrywasz!');
+  	 } else {
+    	  printMessage('Tym razem przegrywasz :(');
+  	 }
+	}
+	function buttonClicked(){
+  	 printMessage('Guzik został kliknięty');
+        }
+}
+        
