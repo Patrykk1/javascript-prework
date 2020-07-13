92f0f61250c0be812dd2d7080b89084bac98ec75
Patrykk1 <66215919+Patrykk1@users.noreply.github.com>
2020-07-13 15:52:59 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,35 @@
 // JavaScript source code
-function playGame (playerMove){
- clearMessages()
- 
- let randomNumber = Math.floor(Math.random() * 3 + 1);
+function playGame(playerMove) {
+    clearMessages()
 
- console.log('Wylosowana liczba dla komputera to: ' + randomNumber);
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
 
- let computerMove = getMoveName(randomNumber);
- 
- printMessage( 'Komputer wybral ' + computerMove+ ', a gracz wybrał ' + playerMove);
+    console.log('Wylosowana liczba dla komputera to: ' + randomNumber);
+
+    let computerMove = getMoveName(randomNumber);
+
+    printMessage('Komputer wybral ' + computerMove + ', a gracz wybrał ' + playerMove);
     if (computerMove == 'kamień' && playerMove == 'papier') {
-	    printMessage('Ty wygrywasz!');
-	   } else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-	    printMessage('Ty wygrywasz!');
-	   } else if (computerMove == 'papier' && playerMove == 'nożyce') {
-	    printMessage('Ty wygrywasz!');
-	   } else if (computerMove ==  playerMove) {
-	    printMessage('Remis!');
-	   } else {
-	    printMessage('Ty przegrywasz');
-	   }
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove == 'papier' && playerMove == 'nożyce') {
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove == playerMove) {
+        printMessage('Remis!');
+    } else {
+        printMessage('Ty przegrywasz');
+    }
 }
 
- console.log('Gracz wpisa³: ' + playerInput);
+let playRock = document.getElementById('play-rock');
 
- 
+let playPaper = document.getElementById('play-paper');
 
- let playRock = document.getElementById('play-rock');
+let playScissors = document.getElementById('play-scissors');
 
- let playPaper = document.getElementById('play-paper');
+playRock.addEventListener('click', buttonClicked);
 
- let playScissors = document.getElementById('play-scissors');
+playPaper.addEventListener('click', buttonClicked);
 
- playRock.addEventListener('click', buttonClicked);
-
- playPaper.addEventListener('click', buttonClicked);
-
- playScissors.addEventListener('click', buttonClicked);
-
-
-
-
-
-
-
+playScissors.addEventListener('click', buttonClicked);
